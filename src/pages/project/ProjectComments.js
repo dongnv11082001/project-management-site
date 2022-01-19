f3bc67c49feefe7cf3bf7afc7fc51e36633c04b4
doremongu <hiiamgosu4@gmail.com>
2022-01-19 22:16:19 +0700

    update fiiresore rules

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -1,67 +1,73 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import Avatar from '../../components/Avatar'
-import { timestamp } from '../../firebase/config'
-import { useAuthContext } from '../../hooks/useAuthContext'
-import { useFirestore } from '../../hooks/useFirestore'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Avatar from '../../components/Avatar';
+import { timestamp } from '../../firebase/config';
+import { useAuthContext } from '../../hooks/useAuthContext';
+import { useFirestore } from '../../hooks/useFirestore';
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 
 export default function ProjectComments({ project }) {
-    const [newComment, setNewComment] = useState('')
-    const { user } = useAuthContext()
-    const { updateDocument, response } = useFirestore('projects')
+  const [newComment, setNewComment] = useState('');
+  const { user } = useAuthContext();
+  const { updateDocument, response } = useFirestore('projects');
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        const commentToAdd = {
-            displayName: user.displayName,
-            photoURL: user.photoURL,
-            content: newComment,
-            createdAt: timestamp.fromDate(new Date()),
-            id: Math.random() 
-        }
+    const commentToAdd = {
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+      content: newComment,
+      createdAt: timestamp.fromDate(new Date()),
+      id: Math.random(),
+    };
 
-        await updateDocument(project.id, {
-            comments: [...project.comments, commentToAdd],
-        });
+    await updateDocument(project.id, {
+      comments: [...project.comments, commentToAdd],
+    });
 
-        if (!response.error) {
-            setNewComment('')
-        }
+    if (!response.error) {
+      setNewComment('');
     }
+  };
 
-    return (
-        <CommentContainer>
-            <h4>Project comments</h4>
-            <ul>
-                {project.comments.length > 0 && project.comments.map(comment => (
-                    <li key={comment.id}>
-                        <div className='comment-author'>
-                            <Avatar src={comment.photoURL} />
-                            <p>{comment.displayName}</p>
-                        </div>    
-                        <div className='comment-date'>
-                            <p>'date here'</p>
-                        </div>
-                        <div className='comment-content'>
-                            <p>{comment.content}</p>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    <span>Add a new comment</span>
-                    <textarea
-                        required
-                        value={newComment}
-                        onChange={e => setNewComment(e.target.value)}
-                    ></textarea>
-                </label>
-                <button className='btn'>Add comment</button>
-            </form>
-        </CommentContainer>
-    )
+  return (
+    <CommentContainer>
+      <h4>Project comments</h4>
+      <ul>
+        {project.comments.length > 0 &&
+          project.comments.map((comment) => (
+            <li key={comment.id}>
+              <div className="comment-author">
+                <Avatar src={comment.photoURL} />
+                <p>{comment.displayName}</p>
+              </div>
+              <div className="comment-date">
+                <p>
+                  {formatDistanceToNow(comment.createdAt.toDate(), {
+                    addSuffix: true,
+                  })}
+                </p>
+              </div>
+              <div className="comment-content">
+                <p>{comment.content}</p>
+              </div>
+            </li>
+          ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Add a new comment</span>
+          <textarea
+            required
+            value={newComment}
+            onChange={(e) => setNewComment(e.target.value)}
+          ></textarea>
+        </label>
+        <button className="btn">Add comment</button>
+      </form>
+    </CommentContainer>
+  );
 }
 
 const CommentContainer = styled.div`
